880a1f74564f9e55af4e8fdb5f91c3b0a14ebda1
kuraburaman <127852145+kuraburaman@users.noreply.github.com>
2023-10-22 11:28:20 +0900

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,34 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+// 黒枠の画像をロード
+const baseImage = new Image();
+baseImage.crossOrigin = 'anonymous';  // この行を追加
+baseImage.src = 'gatiasari.png';  // 黒枠の画像のパスを指定してください
+baseImage.onload = function() {
+    ctx.drawImage(baseImage, 0, 0, canvas.width, canvas.height);  // この行を修正
+}
+
+function previewImage(input) {
+    const file = input.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const uploadedImage = new Image();
+            uploadedImage.crossOrigin = 'anonymous';  // この行を追加
+            uploadedImage.src = e.target.result;
+            uploadedImage.onload = function() {
+                // ここで赤枠の位置とサイズを指定してください
+                ctx.drawImage(uploadedImage, 326, 213, 100, -111);
+            }
+        }
+        reader.readAsDataURL(file);
+    }
+}
+
+function downloadImage() {
+    const link = document.createElement('a');
+    link.download = 'combined_image.png';
+    link.href = canvas.toDataURL();
+    link.click();
+}
